36c0d37c41278b91d572fd4d9bc5ee5b583e8682
mvaworld <121222210+mvaworld@users.noreply.github.com>
2023-06-15 23:11:44 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Duck Hunting Game
+Welcome to Duck Hunting Game! This game is designed to test your shooting skills. The goal of the game is to shoot as many ducks as possible in 30 seconds. The counter will show you how many targets you hit in the 30s.
+
+## How to Play
+- Use the mouse to aim and click the left mouse button to shoot
+- Shoot as many ducks as possible in 30 seconds
+- The counter will show you how many targets you hit in the 30s
+
+## Tech Used
+- Javascript
+- CSS
+- HTML.
+
+## Installation
+- Clone the repository
+- Open the index.html file in a browser
+
+## Preview Link
+https://www.youtube.com/playlist?list=PL2tpgEP3OLaCHL006kQVUAEfthR6hAf4C
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ js/app.js
@@ -0,0 +1,64 @@
+let count = 0;
+let Timecounter = 60;
+// Logic to Move the Aim Image
+// function moveAimImage(e){
+// let x,y;
+//     if( e.clientX - 35 < 0 || e.clientY - 35 < 0){
+//         x=e.clientX;
+//         y=e.clientY;
+//     }else{
+//         x=e.clientX - 35;
+//         y=e.clientY - 35;
+//     }
+
+//     var imageDiv=document.getElementById("aimImage");
+//     imageDiv.style.left=x + 'px';
+//     imageDiv.style.top=y + 'px';
+// }
+let answer1 = prompt("добро пожаловать ковбой, скажи мне, как тебя зовут?")
+alert('отлично ' + answer1 + ', чтобы пройти испытание тебе нужно набрать 25 очков за 30 секунд, приступай' )
+
+
+setInterval(() => {
+    if (Timecounter >= 0 & count >= 25) {
+        popupBg.classList.add('open');
+        duckImageDiv.classList.add('none');
+    } else {
+        document.getElementById("currentTimer").innerHTML = --Timecounter;
+    }  
+
+    if (Timecounter >= 0 & count <= 25) {
+        document.getElementById("currentTimer").innerHTML = --Timecounter;
+    } else {
+        popupBg2.classList.add('open');
+        duckImageDiv.classList.add('none');
+    }
+
+}, 1000);
+
+function hideBird(clicked_id) {
+    document.getElementById("currentScore").innerHTML = ++count;
+    document.getElementById("fireSound").play();
+    document.getElementById(clicked_id).style.display = "none";
+    let x = Math.floor((Math.random() * 10) + 1)
+    setTimeout(() => {
+        document.getElementById(clicked_id).style.display = "block";
+    }, x * 1000);
+}
+
+document.getElementById("namee").innerHTML = answer1;
+
+// pop-up =========================================
+let popupBg2 = document.querySelector('#pop-up-2');
+let popupBg = document.querySelector('#pop-up-1');
+let popup = document.querySelector('.pop-up__body');
+let openPopupButtons = document.querySelectorAll('.btn');
+let closePopupButton = document.querySelector('.pop-up__close');
+let duckImageDiv = document.querySelector('.duckImageDiv')
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('open');
+    })
+});
